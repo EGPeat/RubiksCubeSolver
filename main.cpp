1292e00e71b32c107d36d8ced4945b0474e24070
EGPeat <87342021+EGPeat@users.noreply.github.com>
2023-06-05 21:42:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,51 +12,26 @@ cout<<"Welcome to Evan's Rubik's Cube Scrambler and Solver Program"<<endl;
 
 rCube RubiksCube;
 RubiksCube.printCube();
+RubiksCube.printCubeCross();
 
-//RubiksCube.yArray[2][6]=colorMap[Color::Green];
-//RubiksCube.yArray[2][2]=colorMap[Color::Blue];
-//RubiksCube.xArray[0][5]=colorMap[Color::Orange];
-//RubiksCube.xArray[0][9]=colorMap[Color::Red];
-//RubiksCube.xMovement(0,3);
+
+//RubiksCube.messUp(20,20);
+//RubiksCube.printCubeCross();
+
+//RubiksCube.printCube();
+
+
+
+
+return 0;
+}
 
 //good generalized testing setup, can modify to check other things
-RubiksCube.xMovement(2,-3);
+/*RubiksCube.xMovement(2,-3);
 RubiksCube.printCube();
 RubiksCube.zMovement(2,3);
 RubiksCube.printCube();
 RubiksCube.xMovement(0,3);
 RubiksCube.printCube();
 RubiksCube.yMovement(0,3);
-RubiksCube.printCube();
-
-
-
-
-
-//RubiksCube.zMovement(2,3);
-//RubiksCube.printCube();
-
-//add in face rotation as well as adding in zmovement
-
-
-
-
-/*
-RubiksCube.xMovement(0,3);
-RubiksCube.printCube();
-RubiksCube.xMovement(2,3);
-RubiksCube.printCube();
-RubiksCube.xMovement(1,3);
-RubiksCube.printCube();
-
-RubiksCube.xMovement(0,-3);
-RubiksCube.printCube();
-RubiksCube.xMovement(2,-3);
-RubiksCube.printCube();
-RubiksCube.xMovement(1,-3);
 RubiksCube.printCube();*/
-//RubiksCube.messUp(20,4);
-
-
-return 0;
-}
