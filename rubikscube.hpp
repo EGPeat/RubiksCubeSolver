149b0b8e307a6970da4d8b51543d4f8d73db918d
EGPeat <87342021+EGPeat@users.noreply.github.com>
2023-06-05 21:43:08 -0700

    Update rubikscube.hpp

--- rubikscube.hpp
+++ rubikscube.hpp
@@ -4,18 +4,18 @@
 #include <array>
 #include <string>
 #include <map>
-//wip
+#include "rubikspiece.hpp"
 enum class Move {
     Right=0,
     RightPr=1,
     Left=2,//have to think really hard about the inputs on these types due to how it's coded
     LeftPr=3,
-    Up=4,
-    UpPr=5,
-    Down=6,
-    DownPr=7,
+    Top=4,
+    TopPr=5,
+    Bottom=6,
+    BottomPr=7,
     Front=8,
-    FrontP=9,
+    FrontPr=9,
     Back=10,
     BackPr=11,
     rotateZ=12,
@@ -25,24 +25,10 @@ enum class Move {
     rotateX=16,
     rotateXPr=17
 };
-enum class Color {
-    White,
-    Red,
-    Blue,
-    Yellow,
-    Orange,
-    Green
-};
 
-std::map<Color, std::string> colorMap = {
-    { Color::White, "\U00002B1C " },
-    //{ Color::White, "\U0001F7E5" }, //brown
-    { Color::Red, "\U0001F7E5" },
-    { Color::Blue, "\U0001F7E6" },
-    { Color::Yellow, "\U0001F7E8" },
-    { Color::Orange, "\U0001F7E7" },
-    { Color::Green, "\U0001F7E9" }
-};
+
+
+
 class rCube
 {
     private:
@@ -50,27 +36,32 @@ class rCube
     //yArray is read top to bottom. From leftmost of white cube (counter clockwise)
     //xArray is read left to right. From bottommost of white cube (clockwise)
     //zArray is read back to front. From leftmost of blue cube (clockwise)
-    std::array<std::array<std::string, 3>, 3> topFace; //blue
-    std::array<std::array<std::string, 3>, 3> bottomFace;//green
-    std::array<std::array<std::string, 3>, 3> firstFace;//white
-    std::array<std::array<std::string, 3>, 3> secondFace;//red
-    std::array<std::array<std::string, 3>, 3> thirdFace;//yellow
-    std::array<std::array<std::string, 3>, 3> fourthFace;//orange
+    std::array<std::array<RubiksPiece, 3>, 3> topFace; //blue
+    std::array<std::array<RubiksPiece, 3>, 3> bottomFace;//green
+    std::array<std::array<RubiksPiece, 3>, 3> firstFace;//white
+    std::array<std::array<RubiksPiece, 3>, 3> secondFace;//red
+    std::array<std::array<RubiksPiece, 3>, 3> thirdFace;//yellow
+    std::array<std::array<RubiksPiece, 3>, 3> fourthFace;//orange
     
     public:
     
     
     rCube();
+    std::array<std::array<RubiksPiece, 3>, 3> setupEdgeType(std::array<std::array<RubiksPiece, 3>, 3> face);
     //import from File
-   // void messUp(int numMoves,int snapShot);
+    //void messUp(int numMoves,int snapShot);
     void printCube();
-    //void movement(Move moveChoice);
+    void printCubeCross();
+   // void movement(Move moveChoice);
     //distanceChoice is in mult of 3
-    void yMovement(int layerChoice, int distanceChoice);//need to work around stuff to rotate
-    void xMovement(int layerChoice, int distanceChoice);
-    void zMovement(int layerChoice, int distanceChoice); //ADD FACE ROTATION
- std::array<std::array<std::string, 3>, 3> rotateFace(std::array<std::array<std::string, 3>, 3> face2Rot, int distanceChoice);
+   // void yMovement(int layerChoice, int distanceChoice);//need to work around stuff to rotate
+   // void xMovement(int layerChoice, int distanceChoice);
+   // void zMovement(int layerChoice, int distanceChoice); //ADD FACE ROTATION
+ //std::array<std::array<std::string, 3>, 3> rotateFace(std::array<std::array<std::string, 3>, 3> face2Rot, int distanceChoice);
 
+
+
+   // void moveWhiteToFirst();
 };
 
 
